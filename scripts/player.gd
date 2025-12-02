8fc30d12a2699e2705b3856b1ff3ba7ee0f18eff
badmuffin <itsabhijeet06@gmail.com>
2025-12-02 15:50:50 +0530

    feat: added collision and world boundary

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends CharacterBody2D
 
 var direction: Vector2 = Vector2(1, 0)
 var speed: int = 100
@@ -6,3 +6,4 @@ var speed: int = 100
 func _physics_process(delta: float) -> void:
 	direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	position += direction * speed * delta
+	move_and_slide()
