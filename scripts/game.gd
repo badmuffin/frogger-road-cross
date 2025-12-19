511691b33c21ee6e74d6fe4d815951827cc08a17
badmuffin <itsabhijeet06@gmail.com>
2025-12-20 00:10:24 +0530

    feat: title screen for game over

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,10 +1,13 @@
 extends Node2D
 
 var car_scene: PackedScene = preload("res://scenes/car.tscn")
+var score: int = 0
 
-func _on_finish_area_body_entered(body: Node2D) -> void:
-	print(body)
-	print('has entered')
+func _on_finish_area_body_entered(_body: Node2D) -> void:
+	call_deferred("change_scene")
+
+func change_scene():
+	get_tree().change_scene_to_file("res://scenes/title.tscn")
 
 func _on_car_timer_timeout() -> void:
 	var car: Area2D = car_scene.instantiate()
@@ -16,4 +19,8 @@ func _on_car_timer_timeout() -> void:
 	car.connect("body_entered", go_to_title)
 	
 func go_to_title(_body: Node2D):
-	print("Player collision")
+	call_deferred("change_scene")
+
+func _on_score_timer_timeout() -> void:
+	score += 1
+	$CanvasLayer/Label.text = "time: " + str(score)
