d31ea8378b86fa3de4fbe6e4918e4c7493b8e07e
badmuffin <itsabhijeet06@gmail.com>
2025-12-23 23:04:26 +0530

    feat: added score logic and ui

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,24 +1,41 @@
 extends Node2D
 
 var car_scene: PackedScene = preload("res://scenes/car.tscn")
-var score: int = 0
+var score: int
+var is_game_over := false
 
 func _on_finish_area_body_entered(_body: Node2D) -> void:
+	if is_game_over:
+		return
+
+	is_game_over = true
+	if Global.score == 0 or score < Global.score:
+		Global.score = score
 	call_deferred("change_scene")
 
 func change_scene():
+	if not is_inside_tree():
+		return
+
 	get_tree().change_scene_to_file("res://scenes/title.tscn")
 
 func _on_car_timer_timeout() -> void:
+	if is_game_over:
+		return
+
 	var car: Area2D = car_scene.instantiate()
 	
 	var car_spawer_pos: Marker2D = $CarSpawner.get_children().pick_random()
 	car.position = car_spawer_pos.position
 	
 	$Object.add_child(car)
-	car.connect("body_entered", go_to_title)
+	car.body_entered.connect(go_to_title)
 	
 func go_to_title(_body: Node2D):
+	if is_game_over:
+		return
+	
+	is_game_over = true
 	call_deferred("change_scene")
 
 func _on_score_timer_timeout() -> void:
---
+++ scripts/title.gd
@@ -0,0 +1,9 @@
+extends Control
+
+func _ready() -> void:
+	$Label2.text = "Record time: " + str(Global.score)
+	#Global.score = 0
+
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("ui_accept"):
+		get_tree().change_scene_to_file("res://scenes/game.tscn")
